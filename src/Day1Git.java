e770c49d5ca846d16bbc39d4454c5c9ccbc4ce87
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2022-08-13 21:58:41 +0300

    3.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,5 +2,6 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Git 1. Deneme");
         System.out.println("Git 2. Deneme");
+        System.out.println("Git 3. Deneme");
     }
 }
